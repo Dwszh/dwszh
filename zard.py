febf9f1ec960a5cf665057a8a486a576e2f2d3c2
Dwszh <129860971+Dwszh@users.noreply.github.com>
2023-04-05 14:20:42 +0200

    Update zard.py

--- zard.py
+++ zard.py
@@ -1,5 +1,5 @@
 import telebot
-bot=telebot.TeleBot('token')
+bot=telebot.TeleBot('6238394380:AAG3WwGvOBnWVoNj3b7YBM28I7yxORjzpAo')
 @bot.message_handler(commands=['start'])
 def s(message):
     bot.send_message(message.chat.id, "Привет!")
