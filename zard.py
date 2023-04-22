50fe03408a8bec65439d5bb67f3c3b4701e60dc6
Dwszh <129860971+Dwszh@users.noreply.github.com>
2023-04-22 12:35:18 +0200

    Delete zard.py

--- zard.py
+++
@@ -1,6 +0,0 @@
-import telebot
-bot=telebot.TeleBot('6238394380:AAG3WwGvOBnWVoNj3b7YBM28I7yxORjzpAo')
-@bot.message_handler(commands=['start'])
-def s(message):
-    bot.send_message(message.chat.id, "Привет!")
-bot.infinity_polling() 
